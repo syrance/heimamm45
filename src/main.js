797429417bb3f0048205fe9e160b294397a868a3
syrance <58377869+syrance@users.noreply.github.com>
2020-01-02 14:51:08 +0800

    element-ui

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
+//导入element-ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+//注册
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ const router = new VueRouter({
         },
         {
             path:'/index',
-            component:login
+            component:index
         },
     ]
 })
